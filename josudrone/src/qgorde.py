c7d116acbe56782648d204799e85a324e85dd586
josuu11 <113379489+josuu11@users.noreply.github.com>
2022-09-12 20:41:49 +0200

    Delete josudrone directory

--- josudrone/src/qgorde.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-fitxategia="taula.npy"
-q={365,2,7}
-np.save(fitxategia,q)
--- josudrone/src/qkargatu.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-fitxategia = "taula.npy"
-q = {}
-q = np.load(fitxategia,allow_pickle=True)
-print (q)
